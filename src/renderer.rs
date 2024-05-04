6fda7bf155b97e748cedd66769870c63c2349e56
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-04 20:26:16 +0800

    Added comments and buffer parameter

--- src/renderer.rs
+++ src/renderer.rs
@@ -4,7 +4,11 @@ pub struct Renderer {
 }
 
 impl Renderer {
-    pub fn render(scene: Scene) {
+    pub fn render(scene: Scene, buffer: &mut [u32]) {
+        // Read scene
+        // Open and load 3D objects
+        // Perform rasterization
+        // Draw to buffer
     }
 
     pub fn new() -> Self {
