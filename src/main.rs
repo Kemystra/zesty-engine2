3c553b9cb642c55e8afbf16255b38ada7649d538
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-04 12:37:47 +0800

    Removed unused comments

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ use app::App;
 fn main() {
     let event_loop = EventLoop::new().unwrap();
 
-    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
-    // dispatched any events. This is ideal for games and similar applications.
     event_loop.set_control_flow(ControlFlow::Poll);
 
     let mut app = App::default();
