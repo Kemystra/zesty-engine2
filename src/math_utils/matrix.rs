ef3ed7a642984ee03d545cb0c49128a7e337a5f9
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-08 10:14:22 +0800

    Replaced Matrix internals with array instead of Vectors
    Too much complexity with Vectors: hard to implement Index later and automatically build identity Matrices via macros

--- src/math_utils/matrix.rs
+++ src/math_utils/matrix.rs
@@ -1,20 +1,11 @@
 use zesty_engine2_derive::Matrix;
-use super::vector::{Vector3, Vector4};
 
 
 pub(super) trait Matrix {}
 
 #[derive(Matrix)]
-pub struct Matrix3 {
-    x: Vector3<f32>,
-    y: Vector3<f32>,
-    z: Vector3<f32>
-}
+pub struct Matrix3([[f32; 3]; 3]);
 
 #[derive(Matrix)]
-pub struct Matrix4 {
-    x: Vector4<f32>,
-    y: Vector4<f32>,
-    z: Vector4<f32>,
-    w: Vector4<f32>
+pub struct Matrix4([[f32; 4]; 4]);
 }
