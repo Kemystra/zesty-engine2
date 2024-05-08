3dd5c4bde14b872615a479e0704c90671fa3c285
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-08 10:26:40 +0800

    Removed Matrix trait derive macro
    Too much complexity for little benefit. Plus possible maintenance cost later

--- zesty_engine2_derive/src/lib.rs
+++ zesty_engine2_derive/src/lib.rs
@@ -1,14 +1,3 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{DeriveInput, parse_macro_input};
-
-
-#[proc_macro_derive(Matrix)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
-        impl Matrix for #ident {}
-    };
-
-    output.into()
-}
