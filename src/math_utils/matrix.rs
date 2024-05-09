989368901df024427c1c6e0697e8f3c1b0b3ed42
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-09 14:48:16 +0800

    Implemented Matrix trait to Matrix3 and Matrix4

--- src/math_utils/matrix.rs
+++ src/math_utils/matrix.rs
@@ -1,11 +1,19 @@
 use zesty_engine2_derive::Matrix;
 
 
-pub(super) trait Matrix {}
 
-#[derive(Matrix)]
+pub(super) trait Matrix: Sized {
+    const SIZE: usize;
+    const IDENTITY: Self;
+
+    fn invert(&self) -> Result<Self, String>;
+}
+
+#[derive(Matrix, Clone)]
+#[matrix(3)]
 pub struct Matrix3([[f32; 3]; 3]);
 
-#[derive(Matrix)]
+#[derive(Matrix, Clone)]
+#[matrix(4)]
 pub struct Matrix4([[f32; 4]; 4]);
 }
