d06db7437d2b4b3ac01acfecdef98b7c2b54c6bd
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-23 12:28:41 +0800

    Updated elementary row operation functions to use new Matrix<N>

--- src/math_utils/matrix.rs
+++ src/math_utils/matrix.rs
@@ -27,26 +27,24 @@ impl<const N: usize> IndexMut<usize> for Matrix<N> {
 
 // 3 functions that perform the elementary row operations needed for inverting a matrix
 // 1. Swapping rows
-fn matrix_swap_row<T, const N: usize>(
-    matrix: &mut T,
+fn matrix_swap_row<const N: usize>(
+    matrix: &mut Matrix<N>,
     row_index_1: usize,
     row_index_2: usize
-) -> ()
-where T: Matrix + Index<usize, Output = [f32; N]> {
+) -> () {
     let tmp = matrix[row_index_1].clone();
     matrix[row_index_1] = matrix[row_index_2];
     matrix[row_index_2] = tmp;
 }
 
 // 2. Add rows (possibly with a multiplier)
-fn matrix_add_multiply_row<T, const N: usize>(
-    matrix: &mut T,
+fn matrix_add_multiply_row<const N: usize>(
+    matrix: &mut Matrix<N>,
     target_row_index: usize,
     add_row_index: usize,
     multiplier: f32,
-) -> ()
-where T: Matrix + Index<usize, Output = [f32; N]> {
-    let columns = T::SIZE;
+) -> () {
+    let columns = Matrix::<N>::SIZE;
 
     for col in 0..columns {
         matrix[target_row_index][col] += multiplier * matrix[add_row_index][col];
@@ -54,13 +52,12 @@ where T: Matrix + Index<usize, Output = [f32; N]> {
 }
 
 // 3. Multiply the whole row with a scalar value
-fn matrix_scale_row<T, const N: usize>(
-    matrix: &mut T,
+fn matrix_scale_row<const N: usize>(
+    matrix: &mut Matrix<N>,
     target_row_index: usize,
     multiplier: f32
-) -> ()
-where T: Matrix + Index<usize, Output = [f32; N]> {
-    let columns = T::SIZE;
+) -> () {
+    let columns = Matrix::<N>::SIZE;
 
     for col in 0..columns {
         matrix[target_row_index][col] *= multiplier;
