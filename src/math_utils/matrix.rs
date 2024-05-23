5794177ba51a3cf195ecd7793a2969c575748ea8
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-23 12:25:32 +0800

    Removed Matrix trait in favor of Matrix struct with const generic
    Avoid the hassle of numerous trait bounds that can't be packaged as supertraits

--- src/math_utils/matrix.rs
+++ src/math_utils/matrix.rs
@@ -1,25 +1,19 @@
-use zesty_engine2_derive::Matrix;
-
 use std::ops::Index;
 use std::ops::IndexMut;
 
-
-pub(super) trait Matrix: Sized + Clone + Index<usize> + IndexMut<usize> {
-    const SIZE: usize;
-    const IDENTITY: Self;
-
-    fn invert(&self) -> Result<Self, String>;
-}
 
 macro_rules! impl_index_mut_matrices {
     ($Matrix:ident, $size:expr) => {
          impl Index<usize> for $Matrix {
             type Output = [f32; $size];
+struct Matrix<const N: usize>([[f32; N]; N]);
 
             fn index(&self, index: usize) -> &Self::Output {
                 &self.0[index]
             }
         }
+pub type Matrix3 = Matrix<3>;
+pub type Matrix4 = Matrix<4>;
 
         impl IndexMut<usize> for $Matrix {
             fn index_mut(&mut self, index: usize) -> &mut Self::Output {
@@ -27,19 +21,13 @@ macro_rules! impl_index_mut_matrices {
             }
         }
     };
+impl<const N: usize> Matrix<N> {
+    const SIZE: usize = N;
 }
 
-#[derive(Matrix, Clone)]
-#[matrix(3)]
-pub struct Matrix3([[f32; 3]; 3]);
 
-impl_index_mut_matrices!(Matrix3, 3);
 
-#[derive(Matrix, Clone)]
-#[matrix(4)]
-pub struct Matrix4([[f32; 4]; 4]);
 
-impl_index_mut_matrices!(Matrix4, 4);
 // 3 functions that perform the elementary row operations needed for inverting a matrix
 // 1. Swapping rows
 fn matrix_swap_row<T, const N: usize>(
