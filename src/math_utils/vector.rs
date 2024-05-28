74973dde6dcea563ed530752d692f43d4bd194d3
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-28 17:42:12 +0800

    Removed Vector4 and trait bounds on each Vector* structs
    We don't have a need for Vector4 now, and trait bounds on structs might be harmful

--- src/math_utils/vector.rs
+++ src/math_utils/vector.rs
@@ -1,20 +1,13 @@
 use num_traits::Num;
 
 
-pub struct Vector4<T: Num> {
-    x: T,
-    y: T,
-    z: T,
-    w: T
-}
-
-pub struct Vector3<T: Num> {
+pub struct Vector3<T> {
     x: T,
     y: T,
     z: T
 }
 
-pub struct Vector2<T: Num> {
+pub struct Vector2<T> {
     x: T,
     y: T,
 }
