cb2b82b193f9814084f7d3d886d16c505816634d
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-30 19:34:23 +0800

    Added type alias for small vectors

--- src/math_utils/vector.rs
+++ src/math_utils/vector.rs
@@ -3,6 +3,10 @@ use super::Float;
 
 pub struct Vector<const N: usize>([Float; N]);
 
+pub type Vector3 = Vector<3>;
+pub type Vector4 = Vector<4>;
+pub type Vector2 = Vector<2>;
+
 impl<const N: usize> Vector<N> {
     pub fn x(&self) -> Float {
         
