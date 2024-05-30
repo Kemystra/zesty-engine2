11ea8360afab25302b3a12188968845d2cc26449
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-30 16:35:49 +0800

    Adapted Vector to newtype arrays with compile-time size

--- src/math_utils/vector.rs
+++ src/math_utils/vector.rs
@@ -1,16 +1,4 @@
 use num_traits::Num;
 
 
-pub(crate) trait Vector {
-}
-
-pub struct Vector3<T> {
-    x: T,
-    y: T,
-    z: T
-}
-
-pub struct Vector2<T> {
-    x: T,
-    y: T,
-}
+pub struct Vector<const N: usize>([f32])
