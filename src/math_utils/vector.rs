f86ef98d10991a28b162e84e2ebb88edcbda97e3
Kemystra <programmer.coleslaw981@slmail.me>
2024-05-30 19:34:51 +0800

    Added getter and setter for vector components according to typical mathematical convention

--- src/math_utils/vector.rs
+++ src/math_utils/vector.rs
@@ -9,6 +9,26 @@ pub type Vector2 = Vector<2>;
 
 impl<const N: usize> Vector<N> {
     pub fn x(&self) -> Float {
-        
+        self.0[0]
+    }
+
+    pub fn y(&self) -> Float {
+        self.0[1]
+    }
+
+    pub fn z(&self) -> Float {
+        self.0[2]
+    }
+
+    pub fn set_x(&mut self, value: Float) -> () {
+        self.0[0] = value;
+    }
+
+    pub fn set_y(&mut self, value: Float) -> () {
+        self.0[1] = value;
+    }
+
+    pub fn set_z(&mut self, value: Float) -> () {
+        self.0[2] = value;
     }
 }
