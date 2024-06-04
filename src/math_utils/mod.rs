1b70b9be7ac4411d060d0dba99dd5058a4fff650
Kemystra <programmer.coleslaw981@slmail.me>
2024-06-04 15:09:17 +0800

    Added test for transform_3d_vector()

--- src/math_utils/mod.rs
+++ src/math_utils/mod.rs
@@ -4,3 +4,24 @@ pub mod quaternion;
 
 
 pub type Float = f32;
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_transform_3d_vector() {
+        let matrix = Matrix4::new([
+            [1.0, 0.0, 0.0, 0.0], // Translation by (2, 3, 4)
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [2.0, 3.0, 4.0, 1.0],
+        ]);
+
+        let vector = Vector3::new([1.0, 2.0, 3.0]);
+
+        let result = transform_3d_vector(&matrix, vector);
+
+        let expected = Vector3::new([3.0, 5.0, 7.0]);
+        assert_eq!(result.array(), expected.array());
+    }
+}
