54bb178aeca0501d8f82687160323d0049718c84
Kemystra <programmer.coleslaw981@slmail.me>
2024-06-05 19:29:49 +0800

    Implemented PartialEq trait for testing Quaternions

--- src/math_utils/quaternion.rs
+++ src/math_utils/quaternion.rs
@@ -1,4 +1,15 @@
+use std::cmp::PartialEq;
+
 use crate::math_utils::FloatType;
 
 
 pub struct Quaternion([FloatType; 4]);
+
+#[cfg(test)]
+impl PartialEq for Quaternion {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.iter()
+            .zip(other.0.iter())
+            .all(|(a,b)| (a-b).abs() < FloatType::EPSILON)
+    }
+}
