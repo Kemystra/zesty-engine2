177e71945f6ca107e9ca0b8ab679095c34c57373
Kemystra <programmer.coleslaw981@slmail.me>
2024-06-09 14:34:16 +0800

    Fixed incompatible types in unit tests of from_euler_angles()

--- src/math_utils/quaternion.rs
+++ src/math_utils/quaternion.rs
@@ -44,25 +44,25 @@ mod tests {
 
     #[test]
     fn quaternion_from_euler_angle_x_only() {
-        let q = Quaternion::from_euler_angles(1,0,0);
+        let q = Quaternion::from_euler_angles(1.0, 0.0, 0.0);
         assert_eq!(q, Quaternion([0.87758, 0.47943, 0.0, 0.0]));
     }
 
     #[test]
     fn quaternion_from_euler_angle_y_only() {
-        let q = Quaternion::from_euler_angles(0,1,0);
+        let q = Quaternion::from_euler_angles(0.0, 1.0, 0.0);
         assert_eq!(q, Quaternion([0.87758, 0.0, 0.47943, 0.0]));
     }
 
     #[test]
     fn quaternion_from_euler_angle_z_only() {
-        let q = Quaternion::from_euler_angles(0,0,1);
+        let q = Quaternion::from_euler_angles(0.0, 0.0, 1.0);
         assert_eq!(q, Quaternion([0.87758, 0.0, 0.0, 0.47943]));
     }
 
     #[test]
     fn quaternion_from_euler_angle_all() {
-        let q = Quaternion::from_euler_angles(1,1,1);
+        let q = Quaternion::from_euler_angles(1.0, 1.0, 1.0);
         assert_eq!(q, Quaternion([0.78607, 0.16752, 0.57094, 0.16752]));
     }
 }
