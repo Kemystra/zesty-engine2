b630f755c5cdc072f9a5465cfa4fddb50d8897be
Kemystra <programmer.coleslaw981@slmail.me>
2024-06-24 16:37:44 +0800

    Added more conventions to follow for orientation of objects

--- src/math_utils/mod.rs
+++ src/math_utils/mod.rs
@@ -11,6 +11,8 @@ pub type FloatType = f32;
 // IMPORTANT!
 // Any part of code that deals with angles should be assumed to use radians
 // All matrix are row-major
+// Coordinates are RIGHT-HANDED ffs, opposite to the original raster-3d-engine
+// Any MENTION of Euler angles are assumed to be intrinsic Tait-Bryan angles with sequence XYZ
 
 // Applies 3D transformations represented as Matrix4 to a point represented as Vector3.
 // This includes rotations, scaling, and translations
