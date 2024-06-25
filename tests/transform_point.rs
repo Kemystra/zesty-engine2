c8f3f4b2ab77f064d4cdbfe25d4be52a7b3704db
Kemystra <programmer.coleslaw981@slmail.me>
2024-06-25 16:03:52 +0800

    Changed import of generic Matrix to Matrix4 type alias instead int transform_points tests

--- tests/transform_point.rs
+++ tests/transform_point.rs
@@ -2,13 +2,14 @@ use zesty_engine2::math_utils;
 use math_utils::{transform_3d_point, vector, matrix, quaternion};
 use quaternion::Quaternion;
 use vector::prelude::*;
-use matrix::Matrix;
+use matrix::Matrix4;
+
 
 
 #[test]
 fn test_translate_point() {
     let point = vector![1.0, 0.0, 1.0];
-    let mut matrix = Matrix::<4>::identity_matrix();
+    let mut matrix = Matrix4::identity_matrix();
 
     matrix[3] = [2.4, 5.6, 90.1, 1.0];
     let new_point = transform_3d_point(&matrix, point);
