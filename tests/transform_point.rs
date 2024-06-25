fe5ffc73eb45f13df4d8caeac262a1a8f5691b20
Kemystra <programmer.coleslaw981@slmail.me>
2024-06-25 15:18:12 +0800

    Made first successful integration test for 3D point transformations

---
+++ tests/transform_point.rs
@@ -0,0 +1,16 @@
+use zesty_engine2::math_utils;
+use math_utils::{transform_3d_point, vector, matrix, quaternion};
+use quaternion::Quaternion;
+use vector::prelude::*;
+use matrix::Matrix;
+
+
+#[test]
+fn test_translate_point() {
+    let point = vector![1.0, 0.0, 1.0];
+    let mut matrix = Matrix::<4>::identity_matrix();
+
+    matrix[3] = [2.4, 5.6, 90.1, 1.0];
+    let new_point = transform_3d_point(&matrix, point);
+    assert_eq!(new_point, vector![3.4, 5.6, 91.1]);
+}
