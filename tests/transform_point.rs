85f4e249f64855a377b63e8ed1aa400d47800a1f
Kemystra <programmer.coleslaw981@slmail.me>
2024-06-27 14:49:40 +0800

    Added integration test to check for rotation of point around each axis

--- tests/transform_point.rs
+++ tests/transform_point.rs
@@ -26,3 +26,30 @@ fn rotation_test_helper(point: Vector3<FloatType>, rot: Quaternion, expected_poi
     let new_point = transform_3d_point(&matrix, point);
     assert_eq!(new_point, expected_point);
 }
+
+#[test]
+fn test_rotate_point_around_x_axis() {
+    rotation_test_helper(
+        vector![0.0, 1.0, 0.0],
+        Quaternion::from_euler_angles(DEG_45, 0.0, 0.0),
+        vector![0.0, 0.7071067812, 0.7071067812]
+    )
+}
+
+#[test]
+fn test_rotate_point_around_y_axis() {
+    rotation_test_helper(
+        vector![1.0, 0.0, 0.0],
+        Quaternion::from_euler_angles(0.0, DEG_45, 0.0),
+        vector![0.7071067812, 0.0, -0.7071067812]
+    )
+}
+
+#[test]
+fn test_rotate_point_around_z_axis() {
+    rotation_test_helper(
+        vector![0.0, 1.0, 0.0],
+        Quaternion::from_euler_angles(0.0, 0.0, DEG_45),
+        vector![-0.7071067812, 0.7071067812, 0.0]
+    )
+}
