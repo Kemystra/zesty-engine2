68e5f686bdc848f77eaacaef4ace698be618c59b
Kemystra <programmer.coleslaw981@slmail.me>
2024-06-27 14:54:45 +0800

    Updated matrix orientation convention to column-major
    Made more sense in terms of math, plus adjusting Quaternion's edit_matrix() method gonna suck

--- src/math_utils/mod.rs
+++ src/math_utils/mod.rs
@@ -10,7 +10,7 @@ pub type FloatType = f32;
 
 // IMPORTANT!
 // Any part of code that deals with angles should be assumed to use radians
-// All matrix are row-major
+// All matrix are COLUMN-MAJOR and vectors are POST-MULTIPLIED with matrices.
 // Coordinates are RIGHT-HANDED ffs, opposite to the original raster-3d-engine
 // Any MENTION of Euler angles are assumed to be intrinsic Tait-Bryan angles with sequence
 // Z-Y'-X''
