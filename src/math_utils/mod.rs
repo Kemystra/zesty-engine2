f3e93f0c8501b8a3219b7e28c71d29b78674bbd5
Kemystra <programmer.coleslaw981@slmail.me>
2024-07-11 11:33:39 +0800

    Fking beautified a small comment in mod.rs for math_utils

--- src/math_utils/mod.rs
+++ src/math_utils/mod.rs
@@ -13,7 +13,7 @@ pub type FloatType = f32;
 // All matrix are COLUMN-MAJOR and vectors are POST-MULTIPLIED with matrices.
 // Coordinates are RIGHT-HANDED ffs, opposite to the original raster-3d-engine
 // Any MENTION of Euler angles are assumed to be intrinsic Tait-Bryan angles with sequence
-// Z-Y'-X''
+// Z-Y'-X"
 // A rotation is POSITIVE if it moves clockwise when looking in the positive direction of its axis
 
 // Applies 3D transformations represented as Matrix4 to a point represented as Vector3.
