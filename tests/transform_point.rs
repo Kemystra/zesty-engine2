0f3cf5f749a8af2f7ac40e018cb9a354a2e5f2d0
Kemystra <programmer.coleslaw981@slmail.me>
2024-08-19 18:53:31 +0800

    Added ApproxCmp trait as a blanket implementation of MathStruct
    Also fixed integration tests errors

--- tests/transform_point.rs
+++ tests/transform_point.rs
@@ -1,7 +1,7 @@
 use std::f32::consts::FRAC_PI_4;
 
 use zesty_engine2::math_utils;
-use math_utils::{transform_3d_point, vector, matrix, quaternion, FloatType};
+use math_utils::{transform_3d_point, vector, matrix, quaternion, FloatType, ApproxCmp};
 use quaternion::Quaternion;
 use vector::prelude::*;
 use matrix::Matrix4;
@@ -14,9 +14,13 @@ fn test_translate_point() {
     let point = vector![1.0, 0.0, 1.0];
     let mut matrix = Matrix4::identity_matrix();
 
-    matrix[3] = [2.4, 5.6, 90.1, 1.0];
+    matrix[0][3] = 2.4;
+    matrix[1][3] = 5.6;
+    matrix[2][3] = 90.1;
+
     let new_point = transform_3d_point(&matrix, point);
-    assert_eq!(new_point, vector![3.4, 5.6, 91.1]);
+    println!("{:?}", new_point);
+    new_point.approx_cmp(vector![3.4, 5.6, 91.1], (1.0, 2));
 }
 
 fn rotation_test_helper(point: Vector3<FloatType>, rot: Quaternion, expected_point: Vector3<FloatType>) {
@@ -24,7 +28,7 @@ fn rotation_test_helper(point: Vector3<FloatType>, rot: Quaternion, expected_poi
     rot.edit_3d_matrix(&mut matrix, Vector3::one());
 
     let new_point = transform_3d_point(&matrix, point);
-    assert_eq!(new_point, expected_point);
+    new_point.approx_cmp(expected_point, (1.0, 2));
 }
 
 #[test]
