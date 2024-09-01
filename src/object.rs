f7f1f65939c25f7634ac2ac068def4a0da44ffed
Kemystra <programmer.coleslaw981@slmail.me>
2024-09-01 23:17:13 +0800

    Added custom error wrapper for Obj

--- src/object.rs
+++ src/object.rs
@@ -1,7 +1,8 @@
-use std::{error::Error, fs::File};
-use std::io::BufReader;
+use std::fs::File;
+use std::io::{BufReader, self};
+use std::fmt::Display;
 
-use obj::{Obj, load_obj};
+use obj::{Obj, load_obj, ObjError};
 
 use crate::transform::Transform;
 
@@ -11,8 +12,34 @@ pub struct Object {
     mesh: Obj
 }
 
+pub enum ObjectError {
+    IOError(io::Error),
+    ObjError(ObjError)
+}
+
+impl From<io::Error> for ObjectError {
+    fn from(value: io::Error) -> Self {
+        Self::IOError(value)
+    }
+}
+
+impl From<ObjError> for ObjectError {
+    fn from(value: ObjError) -> Self {
+        Self::ObjError(value)
+    }
+}
+
+impl Display for ObjectError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::IOError(err) => err.fmt(f),
+            Self::ObjError(err) => err.fmt(f)
+        }
+    }
+}
+
 impl Object {
-    pub fn new(filename: String) -> Result<Self, Box<dyn Error>> {
+    pub fn new(filename: String) -> Result<Self, ObjError> {
         let input = BufReader::new(File::open(filename)?);
         Ok(Self {
             transform: Transform::default(),
