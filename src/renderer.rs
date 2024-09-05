a2927b0de3a9fc14fe723f0c139096a4c3cbb414
Kemystra <programmer.coleslaw981@slmail.me>
2024-09-05 11:40:51 +0800

    Fixed moved Obj.vertices error in Renderer
    Use iter() method instead of into_iter() to ensure using immutable reference only, which is what is passed into the renderr() function

--- src/renderer.rs
+++ src/renderer.rs
@@ -24,7 +24,7 @@ pub struct Renderer {
 
 impl Renderer {
     pub fn render(&self, obj: &Object, buffer: &mut [u32]) {
-        obj.mesh().vertices.into_iter().for_each(|vert| {
+        obj.mesh().vertices.iter().for_each(|vert| {
             let pos = Vector3::new(vert.position);
             let world_pos = obj.transform().local_to_world(pos);
         })
