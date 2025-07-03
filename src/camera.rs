ef2db61191739dbca049221cfb96debc4a0cdd03
k3mystra <programmer.coleslaw981@slmail.me>
2025-07-03 21:46:28 +0800

    Add ProjectionData

--- src/camera.rs
+++ src/camera.rs
@@ -1,23 +1,55 @@
 use crate::transform::Transform;
-use crate::math_utils::matrix::Matrix4;
 use crate::math_utils::FloatType;
 
 const ASPECT_RATIO : FloatType = 16.0 / 9.0;
 
 pub struct Camera {
     pub transform: Transform,
-    pub projection_matrix: Matrix4,
     near_plane_distance: FloatType,
-    vertical_fov_angle: FloatType
+    far_plane_distance: FloatType,
+    vertical_fov_angle: FloatType,
+    projection_data: ProjectionData
+}
+
+#[derive(Debug)]
+pub struct ProjectionData(FloatType, FloatType, FloatType, FloatType);
+
+impl ProjectionData {
+    pub fn generate<T: Into<FloatType> + Copy>(n: T, f: T, fov: T)
+    -> Self {
+        let n = n.into();
+        let f = f.into();
+        let fov = fov.into();
+
+        let tan_half_pov = (fov * 0.5).tan();
+        let near_far_diff = f - n;
+        Self (
+            // width scaler
+            1.0 / (ASPECT_RATIO * tan_half_pov),
+            // height scaler
+            1.0 / tan_half_pov,
+            // z scaler
+            1.0 / near_far_diff,
+            // z repositioning
+            n / near_far_diff
+        )
+    }
 }
 
 impl Camera {
-    pub fn new(n: FloatType, fov: FloatType) -> Self {
+    pub fn new<T: Into<FloatType> + Copy>(n: T, f: T, fov: T) -> Self {
+        let n = n.into();
+        let f = f.into();
+        let fov = fov.into();
+
         Self {
             transform: Transform::default(),
-            projection_matrix: Matrix4::default(),
             near_plane_distance: n,
-            vertical_fov_angle: fov
+            far_plane_distance: f,
+            vertical_fov_angle: fov,
+            projection_data: ProjectionData::generate(
+                n, f, fov
+            )
         }
     }
 }
