5760adf9f215269836eb82db9ab1f4a4736cf10f
k3mystra <programmer.coleslaw981@slmail.me>
2025-07-03 21:24:14 +0800

    Add FOV angle and near plane dist. into Camera

--- src/camera.rs
+++ src/camera.rs
@@ -1,14 +1,23 @@
 use crate::transform::Transform;
+use crate::math_utils::matrix::Matrix4;
+use crate::math_utils::FloatType;
 
+const ASPECT_RATIO : FloatType = 16.0 / 9.0;
 
 pub struct Camera {
-    pub transform: Transform
+    pub transform: Transform,
+    pub projection_matrix: Matrix4,
+    near_plane_distance: FloatType,
+    vertical_fov_angle: FloatType
 }
 
 impl Camera {
-    pub fn new() -> Self {
+    pub fn new(n: FloatType, fov: FloatType) -> Self {
         Self {
-            transform: Transform::default()
+            transform: Transform::default(),
+            projection_matrix: Matrix4::default(),
+            near_plane_distance: n,
+            vertical_fov_angle: fov
         }
     }
 }
