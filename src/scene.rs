8de6a64fff7d79a7bc0075c2cbf6086c42371871
k3mystra <programmer.coleslaw981@slmail.me>
2025-08-14 15:05:16 +0800

    feat: Apply original position to object in Scene

--- src/scene.rs
+++ src/scene.rs
@@ -1,6 +1,7 @@
 use serde::{Serialize, Deserialize};
 use std::fs;
 
+use crate::math_utils::vector::Vector3;
 use crate::object::Object;
 use crate::math_utils::FloatType;
 
@@ -19,7 +20,9 @@ impl Scene {
     pub fn new(filename: &str) -> Self {
         let contents = fs::read_to_string(filename).unwrap();
         let scene_config: SceneConfig = serde_json::from_str(&contents).unwrap();
-        let object = Object::new(scene_config.filename).unwrap();
+        let mut object = Object::new(scene_config.filename).unwrap();
+        object.transform.set_position(Vector3::<FloatType>::new(scene_config.position));
+        object.transform.update();
 
         Scene {
             object
